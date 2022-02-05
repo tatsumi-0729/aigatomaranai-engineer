269cc1ae41b2b6613c6a59c22db82d0469e1deee
tatsumi <tamura.tatsumii@gmail.com>
2022-02-05 18:17:26 +0900

    fix sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,23 +1,18 @@
 // @flow strict
-import React from 'react';
-import Helmet from 'react-helmet';
-import type { Node as ReactNode } from 'react';
-import { useSiteMetadata } from '../../hooks';
-import styles from './Layout.module.scss';
+import React from "react";
+import Helmet from "react-helmet";
+import type { Node as ReactNode } from "react";
+import { useSiteMetadata } from "../../hooks";
+import styles from "./Layout.module.scss";
 
 type Props = {
   children: ReactNode,
   title: string,
   description?: string,
-  socialImage?: string
+  socialImage?: string,
 };
 
-const Layout = ({
-  children,
-  title,
-  description,
-  socialImage = ''
-}: Props) => {
+const Layout = ({ children, title, description, socialImage = "" }: Props) => {
   const { author, url } = useSiteMetadata();
   const metaImage = socialImage || author.photo;
   const metaImageUrl = url + metaImage;
@@ -25,7 +20,7 @@ const Layout = ({
   return (
     <div className={styles.layout}>
       <Helmet>
-        <html lang="en" />
+        <html lang="jp" />
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta property="og:site_name" content={title} />
