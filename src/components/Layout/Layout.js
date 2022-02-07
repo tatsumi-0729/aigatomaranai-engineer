d52a7e3895f80f4985c599f3148bf63aa6ffb277
tatsumi <tamura.tatsumii@gmail.com>
2022-02-07 18:35:53 +0900

    add react helmet lang ja

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -23,7 +23,7 @@ const Layout = ({ children, title, description, socialImage = "" }: Props) => {
       <Header />
       <div className={styles.layout}>
         <Helmet>
-          <html lang="jp" />
+          <html lang="ja" />
           <title>{title}</title>
           <meta name="description" content={description} />
           <meta property="og:site_name" content={title} />
