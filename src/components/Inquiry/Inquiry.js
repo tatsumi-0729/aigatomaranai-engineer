209ab6160d9a70049c398a271daa656e9b34d8d7
tatsumi <tamura.tatsumii@gmail.com>
2022-02-15 18:32:31 +0900

    fix aitoma-form

--- src/components/Inquiry/Inquiry.js
+++ src/components/Inquiry/Inquiry.js
@@ -1,43 +1,95 @@
 // @flow strict
-import React from "react";
-import styles from "./Inquiry.module.scss";
+import React, { useState } from "react";
+import Helmet from "react-helmet";
+import { stringify } from "qs";
+import { serialize } from "dom-form-serializer";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import styles from "./Inquiry.module.scss";
 
-const Inquiry = () => (
-  <div className={styles["inquiry"]}>
-    <h1 className={styles["inquiry__title"]}>お問い合わせ</h1>
-    <div className={styles["inquiry__body"]}>
-      <Form
-        name="aitoma-form"
-        method="POST"
-        action="/pages/thanks"
-        className="inquiry"
-        data-netlify="true"
-      >
-        <Form.Group className="mb-4" controlId="formBasicEmail">
-          <Form.Label>お名前</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
+const Inquiry = () => {
+  const messageProps = {
+    successMessage: "お問い合わせありがとうございました。",
+    errorMessage:
+      "お問い合わせに失敗しました。お手数ですが再度お問い合わせください。",
+  };
 
-        <Form.Group className="mb-4" controlId="formBasicPassword">
-          <Form.Label>メールアドレス</Form.Label>
-          <Form.Control type="email" />
-        </Form.Group>
+  const [disable, setDisable] = useState(false);
+  const [message, setMessage] = useState("");
 
-        <Form.Group className="mb-5" controlId="formBasicTextarea">
-          <Form.Label>お問い合わせ内容</Form.Label>
-          <Form.Control as="textarea" rows={4} />
-        </Form.Group>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (disable) return;
 
-        <div className="d-grid gap-2 col-6 mx-auto">
-          <Button variant="primary" type="submit" size="lg">
-            送信
-          </Button>
+    const form = e.target;
+    console.log(form);
+    const data = serialize(form);
+    console.log(data);
+    setDisable(true);
+    fetch(form.action + "?" + stringify(data), {
+      method: "POST",
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res;
+        } else {
+          throw new Error("Network error");
+        }
+      })
+      .then(() => {
+        form.reset();
+        setDisable(false);
+        setMessage(messageProps.successMessage);
+      })
+      .catch((err) => {
+        console.error(err);
+        setDisable(false);
+        setMessage(messageProps.errorMessage);
+      });
+  };
+
+  return (
+    <>
+      <Helmet>
+        <script src="https://www.google.com/recaptcha/api.js" />
+      </Helmet>
+      <div className={styles["inquiry"]}>
+        <h1 className={styles["inquiry__title"]}>お問い合わせ</h1>
+        {message && <div className="inquiry__message">{message}</div>}
+        <div className={styles["inquiry__body"]}>
+          <Form
+            name="aitoma-form"
+            method="POST"
+            action="/inquiry"
+            className="inquiry"
+            onSubmit={handleSubmit}
+            data-netlify="true"
+          >
+            <Form.Group className="mb-4" controlId="formBasicEmail">
+              <Form.Label>お名前</Form.Label>
+              <Form.Control name="name" type="text" />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label>メールアドレス</Form.Label>
+              <Form.Control name="email" type="email" />
+            </Form.Group>
+
+            <Form.Group className="mb-5" controlId="formBasicTextarea">
+              <Form.Label>お問い合わせ内容</Form.Label>
+              <Form.Control as="textarea" name="content" rows={4} />
+            </Form.Group>
+
+            <div className="d-grid gap-2 col-6 mx-auto">
+              <Button variant="primary" type="submit" size="lg">
+                送信
+              </Button>
+            </div>
+          </Form>
         </div>
-      </Form>
-    </div>
-  </div>
-);
+      </div>
+    </>
+  );
+};
 
 export default Inquiry;
