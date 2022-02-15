c853f947a24b97c35deeec0d21ad895f8fdbc1d9
tatsumi <tamura.tatsumii@gmail.com>
2022-02-15 20:30:45 +0900

    fix aitoma-form6

--- src/components/Inquiry/Inquiry.js
+++ src/components/Inquiry/Inquiry.js
@@ -1,6 +1,5 @@
 // @flow strict
 import React, { useState } from "react";
-import Helmet from "react-helmet";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import styles from "./Inquiry.module.scss";
@@ -54,61 +53,57 @@ const Inquiry = () => {
   };
 
   return (
-    <>
-      <Helmet>
-        <script src="https://www.google.com/recaptcha/api.js" />
-      </Helmet>
-      <div className={styles["inquiry"]}>
-        <h1 className={styles["inquiry__title"]}>お問い合わせ</h1>
-        {message && <div className="inquiry__message">{message}</div>}
-        <div className={styles["inquiry__body"]}>
-          <Form
-            name="aitoma-form"
-            method="POST"
-            className="inquiry"
-            onSubmit={handleSubmit}
-            netlify
-          >
-            <Form.Group className="mb-4" controlId="formBasicEmail">
-              <Form.Label>お名前</Form.Label>
-              <Form.Control
-                name="name"
-                type="text"
-                value={formState.name}
-                onChange={handleChange}
-              />
-            </Form.Group>
+    <div className={styles["inquiry"]}>
+      <h1 className={styles["inquiry__title"]}>お問い合わせ</h1>
+      {message && <div className="inquiry__message">{message}</div>}
+      <div className={styles["inquiry__body"]}>
+        <Form
+          name="aitoma-form"
+          method="POST"
+          className="inquiry"
+          onSubmit={handleSubmit}
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Label>お名前</Form.Label>
+            <Form.Control
+              name="name"
+              type="text"
+              value={formState.name}
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-            <Form.Group className="mb-4" controlId="formBasicPassword">
-              <Form.Label>メールアドレス</Form.Label>
-              <Form.Control
-                name="email"
-                type="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-            </Form.Group>
+          <Form.Group className="mb-4" controlId="formBasicPassword">
+            <Form.Label>メールアドレス</Form.Label>
+            <Form.Control
+              name="email"
+              type="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-            <Form.Group className="mb-5" controlId="formBasicTextarea">
-              <Form.Label>お問い合わせ内容</Form.Label>
-              <Form.Control
-                as="textarea"
-                name="message"
-                rows={4}
-                value={formState.message}
-                onChange={handleChange}
-              />
-            </Form.Group>
+          <Form.Group className="mb-5" controlId="formBasicTextarea">
+            <Form.Label>お問い合わせ内容</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="message"
+              rows={4}
+              value={formState.message}
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-            <div className="d-grid gap-2 col-6 mx-auto">
-              <Button variant="primary" type="submit" size="lg">
-                送信
-              </Button>
-            </div>
-          </Form>
-        </div>
+          <div className="d-grid gap-2 col-6 mx-auto">
+            <Button variant="primary" type="submit" size="lg">
+              送信
+            </Button>
+          </div>
+        </Form>
       </div>
-    </>
+    </div>
   );
 };
 
