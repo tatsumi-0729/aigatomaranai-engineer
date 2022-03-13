4d6c26e16f3f5cc5303ea793e916b48036acb5b6
tatsumi <tamura.tatsumii@gmail.com>
2022-03-13 18:40:36 +0900

    fix hamburger menu, add accordion

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ import Copyright from "../Sidebar/Copyright";
 import { useSiteMetadata } from "../../hooks";
 
 const Header = () => {
-  const { author, copyright } = useSiteMetadata();
+  const { author, copyright, category, tag } = useSiteMetadata();
 
   return (
     <div className={styles["wrapper"]}>
@@ -48,22 +48,145 @@ const Header = () => {
               className={styles["hamburger-background"]}
             ></label>
             <div className={styles["hamburger-content"]}>
-              <label htmlFor="hamburger-button-check">
-                <div className={styles["hamburger-cross-wrap"]}>
-                  <div></div>
-                  <div></div>
-                  <div className={styles["hamburger-cross"]}>×</div>
+              {/* MENU */}
+              <div className={styles["hamburger-content-menu"]}>
+                <div>MENU</div>
+                <div></div>
+                <label htmlFor="hamburger-button-check">
+                  <div className={styles["hamburger-cross-wrap"]}>
+                    <div className={styles["hamburger-cross"]}>×</div>
+                  </div>
+                </label>
+              </div>
+
+              {/* カテゴリ */}
+              <div className={styles["hamburger-content-category-wrapper"]}>
+                <input
+                  type="checkbox"
+                  id="hamburger-content-category-button-check"
+                  className={styles["hamburger-content-category-button-check"]}
+                ></input>
+                <label
+                  htmlFor="hamburger-content-category-button-check"
+                  className={
+                    styles["hamburger-content-category-icon-text-wrapper"]
+                  }
+                >
+                  <div className={styles["hamburger-content-category-icon"]}>
+                    >
+                  </div>
+                  <div className={styles["hamburger-content-category-text"]}>
+                    カテゴリ
+                  </div>
+                </label>
+                <div
+                  className={
+                    styles["hamburger-content-category-content-wrapper"]
+                  }
+                >
+                  <div
+                    className={
+                      styles[
+                        "hamburger-content-category-content-text-partition"
+                      ]
+                    }
+                  ></div>
+                  {category.map((item) => (
+                    <div>
+                      <div
+                        className={
+                          styles[
+                            "hamburger-content-category-content-text-partition"
+                          ]
+                        }
+                      ></div>
+                      <Link
+                        to={item.path}
+                        className={
+                          styles["hamburger-content-category-content-text"]
+                        }
+                      >
+                        > {item.label}
+                      </Link>
+                      <div
+                        className={
+                          styles[
+                            "hamburger-content-category-content-text-partition"
+                          ]
+                        }
+                      ></div>
+                    </div>
+                  ))}
                 </div>
-              </label>
-              <Author author={author} />
-              <Contacts contacts={author.contacts} />
+              </div>
+
+              {/* タグ */}
+              <div className={styles["hamburger-content-tag-wrapper"]}>
+                <input
+                  type="checkbox"
+                  id="hamburger-content-tag-button-check"
+                  className={styles["hamburger-content-tag-button-check"]}
+                ></input>
+                <label
+                  htmlFor="hamburger-content-tag-button-check"
+                  className={styles["hamburger-content-tag-icon-text-wrapper"]}
+                >
+                  <div className={styles["hamburger-content-tag-icon"]}>></div>
+                  <div className={styles["hamburger-content-tag-text"]}>
+                    タグ
+                  </div>
+                </label>
+                <div
+                  className={styles["hamburger-content-tag-content-wrapper"]}
+                >
+                  <div
+                    className={
+                      styles["hamburger-content-tag-content-text-partition"]
+                    }
+                  ></div>
+                  {tag.map((item) => (
+                    <div>
+                      <div
+                        className={
+                          styles["hamburger-content-tag-content-text-partition"]
+                        }
+                      ></div>
+                      <Link
+                        to={item.path}
+                        className={styles["hamburger-content-tag-content-text"]}
+                      >
+                        > {item.label}
+                      </Link>
+                      <div
+                        className={
+                          styles["hamburger-content-tag-content-text-partition"]
+                        }
+                      ></div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              {/* トップメニュー */}
+              <Link to={"/"} className={styles["hamburger-content-top"]}>
+                トップメニュー
+              </Link>
+
+              {/* お問い合わせ */}
               <Link
                 to={"/inquiry"}
                 className={styles["hamburger-content-inquiry"]}
               >
                 お問い合わせ
               </Link>
-              <Copyright copyright={copyright} />
+
+              <div className={styles["hamburger-content-partition"]}></div>
+              <div className={styles["author-contacts-copyright-wrapper"]}>
+                <Author author={author} />
+                <Contacts contacts={author.contacts} />
+                <div className={styles["hamburger-content-partition"]}></div>
+                <Copyright copyright={copyright} />
+              </div>
             </div>
           </div>
         </div>
